448f34eb91927fc384ffca4006010bd08a632e79
alihan-tadal <tadal.alihan@gmail.com>
2022-05-13 15:27:24 +0300

    write tests

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,4 @@
+from time import time, timezone
 from urllib import response
 from urllib.request import HTTPRedirectHandler
 from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
@@ -7,27 +8,28 @@ from django.http import HttpRequest, Http404
 import pkg_resources
 from django.views import generic
 from .models import Choice, Question
+from django.utils import timezone
+
 # Create your views here.
 
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
-
+    
     def get_queryset(self):
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(
+            pub_date__lte =  timezone.now()).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
 
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte = timezone.now())
+
 class ResultsView(generic.DetailView):
     model = Question
     template_name ='polls/results.html'
-
-
-
-
-
 
 def vote(request,question_id):
     question = get_object_or_404(Question,pk=question_id)
@@ -43,6 +45,5 @@ def vote(request,question_id):
         selected_choice.save()
         print(request.POST)
     return HttpResponseRedirect(reverse('polls:results',args=(question.id,)))
-
 
 
